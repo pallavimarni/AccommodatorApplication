f616d88cdd1254b081e13fb3e4936cc892450f5c
mpandey <my880578@dal.ca>
2023-02-22 20:52:25 -0400

    Basic registration and login page using bootstrap and react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-import Registration from './Components/Registration';
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './Components/Registration'
+import SignUp from './Components/Login'
 function App() {
-  return (
-    <div className="App">
-<Registration></Registration>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                    <div className="container">
+                        <Link className="navbar-brand" to={'/sign-up'}>
+                            ACCOMMODATOR
+                        </Link>
+                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to={'/sign-up'}>
+                                        Login
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to={'/sign-in'}>
+                                        Sign up
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <div className="auth-wrapper">
+                    <div className="form-container">
+
+                    <div className="auth-inner">
+
+                        <Routes>
+                            <Route exact path="/" element={<Login />} />
+                            <Route path="/sign-in" element={<Login />} />
+                            <Route path="/sign-up" element={<SignUp />} />
+                        </Routes>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+        </Router>
+    )
 }
-export default App;
+export default App
--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,29 +1,141 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+import { Form, Button, Dropdown } from 'react-bootstrap';
 
-function BasicExample() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+const RegistrationForm = () => {
+    const [selectedOption, setSelectedOption] = useState('option1');
+    const [validated, setValidated] = useState(false);
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
-}
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
 
-export default BasicExample;
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+    return (
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+
+            <Form.Group>
+                <h3>Sign Up</h3>
+
+                <Form.Check
+                    type="radio"
+                    label="Student"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios1"
+                    value="option1"
+                    checked={selectedOption === 'option1'}
+                    onChange={handleOptionChange}
+                />
+                <Form.Check
+                    type="radio"
+                    label="Owner"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios2"
+                    value="option2"
+                    checked={selectedOption === 'option2'}
+                    onChange={handleOptionChange}
+                />
+            </Form.Group>
+
+            {selectedOption === 'option1' && (
+                <Form.Group controlId="Registration">
+                    <Form.Label>Enter First Name</Form.Label>
+                    <Form.Control type="email" placeholder="Enter First Name" required />
+
+                    <Form.Label>Enter Last Name</Form.Label>
+                    <Form.Control type="email" placeholder="Enter Last Name" required />
+
+                    <Form.Label>Enter Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid email.
+                    </Form.Control.Feedback>
+
+                    <Form.Label>Enter Contact No.</Form.Label>
+                    <Form.Control type="contact" placeholder="Enter Contact No." required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid email.
+                    </Form.Control.Feedback>
+
+                    <Form.Label>Upload Offer Letter</Form.Label>
+                    <Form.Control type="file" placeholder="Upload Offer Letter" required />
+
+                    <Form.Label>Create password</Form.Label>
+                    <Form.Control type="password" placeholder="Create Password" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid password.
+                    </Form.Control.Feedback>
+
+                    <Form.Label>Re-enter Password</Form.Label>
+                    <Form.Control type="password" placeholder="Re-enter Password" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid password.
+                    </Form.Control.Feedback>
+                </Form.Group>
+            )}
+
+            {selectedOption === 'option2' && (
+                <Form.Group controlId="formBasicUsername">
+
+                    <Form.Label>Enter First Name</Form.Label>
+                    <Form.Control type="email" placeholder="Enter First Name" required />
+
+                    <Form.Label>Enter Last Name</Form.Label>
+                    <Form.Control type="email" placeholder="Enter Last Name" required />
+
+                    <Form.Label>Owner type? (Lease or Roommate)</Form.Label>
+                        <select className="form-select" aria-label="Select type">
+                            <option selected>Select type</option>
+                            <option value="1">Student</option>
+                            <option value="2">Owner</option>
+                        </select>
+                    <Form.Control.Feedback type="invalid">
+                        Please Select something.
+                    </Form.Control.Feedback>
+
+
+                    <Form.Label>Enter Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid email.
+                    </Form.Control.Feedback>
+
+                    <Form.Label>Enter Contact No.</Form.Label>
+                    <Form.Control type="contact" placeholder="Enter Contact No." required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid email.
+                    </Form.Control.Feedback>
+
+                    <Form.Label>Create password</Form.Label>
+                    <Form.Control type="password" placeholder="Create Password" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid password.
+                    </Form.Control.Feedback>
+
+                    <Form.Label>Re-enter Password</Form.Label>
+                    <Form.Control type="password" placeholder="Re-enter Password" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid password.
+                    </Form.Control.Feedback>
+                </Form.Group>
+            )}
+
+
+                <div className="d-grid">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+        </Form>
+
+);
+};
+
+export default RegistrationForm;
