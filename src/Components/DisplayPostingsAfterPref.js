84b7e0c24dc3b252be5068e4a09481e680975155
17827 <pl381965@dal.ca>
2023-03-18 15:20:24 -0300

    UI changes

--- src/Components/DisplayPostingsAfterPref.js
+++ src/Components/DisplayPostingsAfterPref.js
@@ -31,43 +31,47 @@ function DisplayPostingsAfterPref() {
     }
     return (
         <div className="auth-wrapper">
-                            <h2 className="text-center text-uppercase text-BLACK mb-4">Latest Postings</h2>
 
-                            <Container>
-                                {posts.map((post, index) => {
-                                    if (index % 3 === 0) {
-                                        const cards = [];
-                                        for (let i = index; i < index + 3 && i < posts.length; i++) {
-                                            const post = posts[i];
-                                            cards.push(
-                                                <Col key={i}>
-                                                    <Card>
-                                                        <button className="favorite-button">
-                                                            <i className="fa fa-heart"></i> Add to favorites
-                                                        </button>
-                                                        <Card.Img variant="top" src={post.image} />
-                                                        <Card.Body>
-                                                            <Card.Title>{post.title}</Card.Title>
-                                                            <Card.Text>{post.description}</Card.Text>
-                                                            <Card.Text>Rent: {post.rent}</Card.Text>
-                                                            <Card.Text>Address: {post.address}, {post.pincode}</Card.Text>
-                                                            <Card.Text>Category: {post.category}</Card.Text>
-                                                            <Button variant="primary" onClick={() => handleConnectClick(post.email)}>Connect with owner</Button>
-                                                        </Card.Body>
-                                                    </Card>
-                                                </Col>
+                                <h2 className="text-center text-uppercase text-BLACK mb-4">Latest Postings</h2>
+
+                                <Container>
+                                    {posts.map((post, index) => {
+                                        if (index % 3 === 0) {
+                                            const cards = [];
+                                            for (let i = index; i < index + 3 && i < posts.length; i++) {
+                                                const post = posts[i];
+                                                cards.push(
+                                                    <Col key={i}>
+                                                        <Card>
+                                                            <button className="favorite-button">
+                                                                <i className="fa fa-heart"></i> Add to favorites
+                                                            </button>
+                                                            <Card.Img variant="top" src={post.image} />
+                                                            <Card.Body>
+                                                                <Card.Title>{post.title}</Card.Title>
+                                                                <Card.Text>{post.description}</Card.Text>
+                                                                <Card.Text>Rent: {post.rent}</Card.Text>
+                                                                <Card.Text>Address: {post.address}, {post.pincode}</Card.Text>
+                                                                <Card.Text>Category: {post.category}</Card.Text>
+                                                                <Button variant="primary" onClick={() => handleConnectClick(post.email)}>Connect with owner</Button>
+                                                            </Card.Body>
+                                                        </Card>
+                                                    </Col>
+                                                );
+                                            }
+                                            return (
+                                                <Row key={index} className="my-3">
+                                                    {cards}
+                                                </Row>
                                             );
                                         }
-                                        return (
-                                            <Row key={index} className="my-3">
-                                                {cards}
-                                            </Row>
-                                        );
-                                    }
-                                })}
-                            </Container>
-        </div>
+                                    })}
+                                </Container>
+                            </div>
+
+
     );
 }
+
 
 export default DisplayPostingsAfterPref;
