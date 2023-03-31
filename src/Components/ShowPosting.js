837cee19670ba7a0ab71d48a03ed4c330ea4af2e
Shaswat Doshi <sh667879@dal.ca>
2023-03-31 15:59:20 -0300

    FE-connect to owner redirection to email

--- src/Components/ShowPosting.js
+++ src/Components/ShowPosting.js
@@ -28,7 +28,13 @@ function ShowPosting() {
 
     function handleConnectClick(email) {
         console.log('Contact with owner on ', email);
+        const subject = "Request for Accommodation";
+        const body = "I am interested in your posting posted in the Accommodator platform. Is it still available?";
+        const mailtoUrl = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+        window.location.href = mailtoUrl;
+
     }
+
 
     function addFavorites(postId) {
         const user = JSON.parse(localStorage.getItem('user'));
